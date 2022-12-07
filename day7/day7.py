e895ff3f56030165529c4b5d12e6c91fe7057e03
Maciej Pfutzner <maciej.pfutzner@intenthq.com>
2022-12-07 15:47:43 +0000

    Finish the good solution for day 7

--- day7/day7.py
+++ day7/day7.py
@@ -1,111 +1,89 @@
-import sys
-from collections import defaultdict
-sys.setrecursionlimit(2000)
-
 lines = [l.strip() for l in open('input.txt').readlines()]
-print(lines[:5])
-
-
-#dirtree = {'/': []}
-dirtree = defaultdict(set)
-#filetree = {'/': []}
-filetree = defaultdict(set)
-
-tree = {'/': {}}
-
-curdir = '/'
-subtree = tree['/']
-
-path = [] # Directories above us
-cursize = 0
-
-def get_subtree(path):
-    subtree = tree
-    for d in path:
-        subtree = subtree[d]
-    return subtree
 
 class Dir:
     def __init__(self, name):
         self.name = name
-        self.files_size = 0
+        self.file_size = 0
         self.subdirs = []
+
+    def __repr__(self):
+        return f'<{self.name}>'
     
     def add_file(self, size):
         self.file_size += size
 
     def add_subdir(self, name):
-        self.subdirs.append(Dir(name))
+        if name not in {s.name for s in self.subdirs}:
+            self.subdirs.append(Dir(name))
 
     def get_subdir(self, name):
         for subdir in self.subdirs:
             if subdir.name == name:
                 return subdir
-
+                                
+root = Dir('/')
+curdir = root
+path = [] # Directories above us
 
 for line in lines:
     cmds = line.split()
-    print(cmds)
+    #print(cmds)
     if cmds[0] == '$':
-        # flush the sizes
-        subtree 
         if cmds[1] == 'cd':
             nextdir = cmds[2] 
-            #if nextdir == '/':
-            #    path = []
-            #    curdir = '/'
-            #    print('Going all the way to /')
-            #elif nextdir == '..':
-            if nextdir == '..':
+            if nextdir == '/':
+                curdir == root
+                path = []
+            elif nextdir == '..':
                 curdir = path.pop()
-                subtree = get_subtree(path)[curdir]
-                print(f'Going up to {curdir}')
+                #print(f'Going up to {curdir}')
             else:
-                curdir = nextdir
                 path.append(curdir)
-                subtree = get_subtree(path)[curdir]
-
-                print(f'Going down to {curdir}')
+                curdir = curdir.get_subdir(nextdir)
+                #print(f'Going down to {curdir}')
         #elif cmds[1] == 'ls':
         #    continue
     elif cmds[0] == 'dir':
         # Is this necessary?
         newdir = cmds[1]
-        dirtree[curdir].add(newdir)
+        curdir.add_subdir(newdir)
     else:
         size, file = cmds
-        filetree[curdir].add(( file, int(size) ))
-        cursize += int(size)
+        curdir.add_file(int(size))
 
-print(dirtree)
-print()
-print(filetree)
-
-counter = 0
+# Determine total sizes of each directory recursively
 sizes = {}
-def find_size(dir_):
-    global counter
-    counter += 1
-    if counter > 20:
-        raise RuntimeError()
-    if dir_ in sizes:
-        return sizes[dir_]
+def find_size(root, verbose=True):
+    if root in sizes:
+        return sizes[root]
 
-    files = list(filetree[dir_])
-    file_size = sum([f[1] for f in files])
-    print(f'Total file size in {dir_} is {file_size}')
+    file_size = root.file_size
+    if verbose:
+        print(f'Total file size in {root.name} is {file_size}')
     
     subdir_size = 0
-    print(f'Subdirs are {dirtree[dir_]}')
-    for subdir in dirtree[dir_]:
-        print(f'Checking subdir {subdir}')
-        subdir_size += find_size(subdir)
+    if verbose:
+        print(f'Subdirs are: {[root.subdirs]}')
+    for subdir in root.subdirs:
+        if verbose:
+            print(f'Checking subdir {subdir}')
+        subdir_size += find_size(subdir, verbose)
 
     total_size = file_size + subdir_size
-    sizes[dir_] = total_size
+    sizes[root] = total_size
     return total_size
 
-find_size('/')
-#find_size('wlmtj')
-#print(sizes)
-                    
+total_size = find_size(root, False)
+print('Total size:', total_size)
+pure_sizes = sorted([sizes[dir_] for dir_ in sizes])
+
+print('Part 1 answer:', sum([s for s in pure_sizes if s <= 100000]))
+
+total_disk = 70000000
+needed = 30000000
+
+free_space = total_disk - total_size
+to_free = needed - free_space
+print('Need to free:', to_free)
+print('Part 2 answer:', [s for s in pure_sizes if s > to_free][0])
+
